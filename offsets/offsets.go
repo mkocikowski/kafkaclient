6286d1855e39e38f3e3bbc304a7e25cba7c9b7b8
Mik Kocikowski <mik@cloudflare.com>
2020-07-21 16:16:58 -0700

    have DumbOffsetsManager disconnect from coordinator on any error
    DumbOffsetsManager Fetch and Commit methods will now close the
    connection to the group coordinator on any error. This is mostly to deal
    with situations where the group coordinator has changed, resulting in
    error code 15. But, like with things based on the PartitionClient
    (producer, consumer), it seems easiest for things based in the
    GroupClient to just close (and re-open on next call) without getting
    into details of what exactly the error is.
    
    Prerequisite for this was adding Close method to GroupClient in
    libkafka.

--- offsets/offsets.go
+++ offsets/offsets.go
@@ -28,6 +28,14 @@ func (c *DumbOffsetsManager) init() {
 	}
 }
 
+// Fetch makes a single FetchOffset api call. If there is no active connection
+// to the group coordinator, it will first look up the coordinator and connect
+// to it (or return en error if unable to do so). Once there is an open
+// connection to the group coordinator, if there is an error making the request
+// or there is an error response (error response code from kafka), the
+// connection to the group coordinator will be closed, and error will be
+// returned. There is no retry logic, it is up to the user (connection will be
+// automatically re-opened on the next call).
 func (c *DumbOffsetsManager) Fetch(topic string, partition int32) (int64, error) {
 	c.init() // idempotent
 	// if partition does not exist or there are no offsets commited
@@ -36,16 +44,20 @@ func (c *DumbOffsetsManager) Fetch(topic string, partition int32) (int64, error)
 	// codes from kafka, etc
 	offset, err := c.client.FetchOffset(topic, partition)
 	if err != nil {
+		c.client.Close() // will reconnect on next call
 		err = kafkaclient.Errorf("error for topic %s partition %d: %w",
 			topic, partition, err)
 	}
 	return offset, err
 }
 
+// Commit makes a single CommitOffset api call. See Fetch documentation for
+// info on error handling.
 func (c DumbOffsetsManager) Commit(topic string, partition int32, offset int64) error {
 	c.init() // idempotent
 	err := c.client.CommitOffset(topic, partition, offset, -1)
 	if err != nil {
+		c.client.Close() // will reconnect on next call
 		err = kafkaclient.Errorf("error for topic %s partition %d: %w",
 			topic, partition, err)
 	}
